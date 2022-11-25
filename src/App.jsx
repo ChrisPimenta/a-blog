766a5558f454fdd9650cd39564c7eb834b497b77
Christopher Pimenta <pimenta.chrisj2@gmail.com>
2022-11-25 13:42:49 +0100

    Handle errors with route - wow :)

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,17 @@ import { RouterProvider, Route, createBrowserRouter, createRoutesFromElements } 
 import BlogLayout from './pages/BlogLayout';
 import BlogPostsPage, { blogPostsLoader } from './pages/BlogPosts';
 import NewPostPage from './pages/NewPost';
-import PostDetailPage from './pages/PostDetail';
+import PostDetailPage, { postDetailPageLoader } from './pages/PostDetail';
 import RootLayout from './pages/RootLayout';
 import WelcomePage from './pages/Welcome';
+import ErrorPage from './pages/Error';
 
 const router = createBrowserRouter(createRoutesFromElements(
-  <Route path='/' element={<RootLayout />}>
+  <Route path='/' element={<RootLayout />} errorElement={<ErrorPage />}>
     <Route index path="/" element={<WelcomePage />} />
     <Route path="/blog" element={<BlogLayout />}>
       <Route index element={<BlogPostsPage />} loader={blogPostsLoader} />
-      <Route path=":id" element={<PostDetailPage />} />
+      <Route path=":id" element={<PostDetailPage />} loader={postDetailPageLoader} />
     </Route>
     <Route path="/blog/new" element={<NewPostPage />} />
   </Route>
